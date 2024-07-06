bb17f0dd3c2114fd1ccedfc850eb2a63ec4a0b4d
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-07-06 13:40:39 +0530

    create a data model class to represent a wish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shooting-star';
+  items = [
+    new WishItem('Build 100 houses in Mexico'),
+    new WishItem('Build 10 schools in Nigeria'),
+    new WishItem('Build 5 football pitches in Argentina')
+  ];
+  title = 'Shooting Stars';
 }
