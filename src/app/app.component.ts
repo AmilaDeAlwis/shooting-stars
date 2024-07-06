9b7ed28353442d573fafedfa0e052f5ad2493037
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-07-06 21:37:15 +0530

    list item completion and index binded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { WishItem } from 'src/shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem('Build 100 houses in Mexico'),
-    // new WishItem('Build 10 schools in Nigeria'),
-    // new WishItem('Build 5 football pitches in Argentina')
+    new WishItem('Build 100 houses in Mexico'),
+    new WishItem('Build 10 schools in Nigeria', true),
+    new WishItem('Build 5 football pitches in Argentina')
   ];
   title = 'Shooting Stars';
 }
