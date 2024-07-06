a3ce6a44eb18883d18e41a399033a3b40711ed50
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-07-06 14:05:52 +0530

    logic added to display items or display no items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { WishItem } from 'src/shared/models/wishItem';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items = [
-    new WishItem('Build 100 houses in Mexico'),
-    new WishItem('Build 10 schools in Nigeria'),
-    new WishItem('Build 5 football pitches in Argentina')
+  items : WishItem[] = [
+    // new WishItem('Build 100 houses in Mexico'),
+    // new WishItem('Build 10 schools in Nigeria'),
+    // new WishItem('Build 5 football pitches in Argentina')
   ];
   title = 'Shooting Stars';
 }
