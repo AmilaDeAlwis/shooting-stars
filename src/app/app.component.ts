203d3c083ca3995fa038c7529bb50abc84cfe7fd
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-07-07 11:52:01 +0530

    a filter added to select different types of wishes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,23 +8,36 @@ import { WishItem } from 'src/shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    // new WishItem('Build 100 houses in Mexico'),
-    // new WishItem('Build 10 schools in Nigeria', true),
-    // new WishItem('Build 5 football pitches in Argentina')
+    new WishItem('Build 100 houses in Mexico'),
+    new WishItem('Build 10 schools in Nigeria', true),
+    new WishItem('Build 5 football pitches in Argentina')
   ];
+
+  title = 'Shooting Stars';
 
   newWishText = '';
 
-  title = 'Shooting Stars';
+  listFilter : String = '0';
+
+  visibleItems : WishItem[] = this.items;
 
   addNewWish(){
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
   }
 
+  filterChanged(value : any){
+    if (value == '0'){
+      this.visibleItems = this.items;
+    } else if (value == '1'){
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    } else {
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
+  }
+
   toggleItem(item : WishItem){
     item.isComplete = !item.isComplete;
     console.log(item);
-    
   }
 }
