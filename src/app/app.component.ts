8df802a4da18e7076a30847909e6940159008a56
AmilaDeAlwis <amilaalwis4@gmail.com>
2024-07-07 12:02:33 +0530

    wish filter updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { WishItem } from 'src/shared/models/wishItem';
 
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,24 +23,17 @@ export class AppComponent {
 
   newWishText = '';
 
-  listFilter : String = '0';
+  listFilter : any = '0';
 
   get visibleItems() : WishItem[]{
-    let value = this.listFilter;
-    if (value == '0'){
-      return this.items;
-    } else if (value == '1'){
-      return this.items.filter(item => !item.isComplete);
-    } else {
-      return this.items.filter(item => item.isComplete);
-    }
+    return this.items.filter(filters[this.listFilter]);
   };
 
   addNewWish(){
     this.items.push(new WishItem(this.newWishText));
     this.newWishText = '';
   }
-  
+
   toggleItem(item : WishItem){
     item.isComplete = !item.isComplete;
     console.log(item);
